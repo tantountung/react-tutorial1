a7c8dc6132e0142814f8b6ebe9a6a38eacfd0c14
tanto untung <untung2k3@yahoo.com>
2021-05-27 15:17:30 +0200

    fiish with forms

---
+++ src/Form.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react'
+
+class Form extends Component {
+  initialState = {
+    name: '',
+    job: '',
+  }
+
+  state = this.initialState
+
+  handleChange = (event) => {
+    const {name, value} = event.target
+  
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  render() {
+    const { name, job } = this.state;
+  
+    return (
+      <form>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={this.handleChange} />
+        <label htmlFor="job">Job</label>
+        <input
+          type="text"
+          name="job"
+          id="job"
+          value={job}
+          onChange={this.handleChange} />
+      </form>
+    );
+
+    <Form handleSubmit={this.handleSubmit} />
+
+    
+
+    submitForm = () => {
+        this.props.handleSubmit(this.state)
+        this.setState(this.initialState)
+      }
+
+      <input type="button" value="Submit" onClick={this.submitForm} />
+  }
+}
+
+  export default Form;
+
+  
